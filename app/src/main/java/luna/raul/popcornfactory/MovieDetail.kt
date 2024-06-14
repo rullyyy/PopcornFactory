2f8eb9c48a8db9cbc21817191bc30302bee9c63d
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-06-13 23:36:04 -0700

    Movie Details

--- app/src/main/java/luna/raul/popcornfactory/MovieDetail.kt
+++ app/src/main/java/luna/raul/popcornfactory/MovieDetail.kt
@@ -1,6 +1,8 @@
 package luna.raul.popcornfactory
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class MovieDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_movie_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        var header: ImageView = findViewById(R.id.movie_header)
+        var title: TextView = findViewById(R.id.movie_title_detail)
+        var desc: TextView = findViewById(R.id.movie_desc)
+
+        val content = intent.extras
+
+        header.setImageResource(content!!.getInt("header"))
+        title.setText(content.getString("titulo"))
+        desc.setText(content.getString("sinopsis"))
     }
 }
